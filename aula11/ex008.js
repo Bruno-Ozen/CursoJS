81c3b702b6dc024dc7ef76c42dec7da801ab701e
Bruno-Özen <112650688+Bruno-Ozen@users.noreply.github.com>
2023-03-30 14:57:42 -0300

    Estrutura condicional simples

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 444.1;
+console.log(`A velocidade do seu carro é ${vel}Km/Hora`);
+if(vel > 40){ // Condição simples
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!!');
+}
+console.log('Se beber, não dirija!');
